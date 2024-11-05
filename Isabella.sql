01114369ca21106c710615c318e3c07bfcc04f66
Isabella Borin <isabellaborin.tds1.senai@gmail.com>
2024-11-05 11:31:34 -0300

    feat: create selects

--- Isabella.sql
+++ Isabella.sql
@@ -19,6 +19,7 @@ id_hospede INT NOT NULL,
 id_quarto INT NOT NULL, 
 data_entrada DATE NOT NULL,
 data_saida DATE NOT NULL,
+reserva_ativa BOOLEAN NOT NULL,
 CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
 CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
 );
@@ -43,12 +44,35 @@ VALUES
 
 SELECT * FROM quartos;
 
-INSERT INTO reservas (id_hospede, id_quarto, data_entrada, data_saida)
+INSERT INTO reservas (id_hospede, id_quarto, data_entrada, data_saida, reserva_ativa)
 VALUES
-(1, 5, '2024-11-05', '2024-11-15'),
-(2, 1, '2024-10-03', '2024-10-09'),
-(3, 2, '2024-11-05', '2024-11-10'),
-(4, 4, '2024-10-31', '2024-11-04'),
-(5, 3, '2024-11-01', '2024-11-10');
+(1, 5, '2024-11-05', '2024-11-15', 'true'),
+(2, 1, '2024-10-03', '2024-10-09', 'false'),
+(3, 2, '2024-11-05', '2024-11-10', 'true'),
+(4, 4, '2024-10-31', '2024-11-04', 'false'),
+(5, 3, '2024-11-01', '2024-11-10', 'true');
 
 SELECT * FROM reservas;
+
+SELECT r.id_reserva, h.nome AS hospede, q.numero_quarto AS quarto, r.data_entrada, r.data_saida
+FROM reservas r
+INNER JOIN hospedes h ON r.id_hospede = h.id_hospede
+INNER JOIN quartos q ON  r.id_quarto = q.id_quarto
+WHERE r.data_saida < CURRENT_DATE;
+
+SELECT r.id_reserva, h.nome AS hospede, q.numero_quarto AS quarto, r.data_entrada, r.data_saida
+FROM reservas r
+INNER JOIN hospedes h ON r.id_hospede = h.id_hospede
+INNER JOIN quartos q ON  r.id_quarto = q.id_quarto;
+
+SELECT r.id_reserva, q.numero_quarto AS quarto_sem_reserva
+FROM reservas r
+JOIN quartos q ON r.id_quarto = q.id_quarto
+WHERE r.data_saida < CURRENT_DATE;
+
+-- ou --
+
+SELECT r.id_reserva, q.numero_quarto AS quarto_sem_reserva, r.reserva_ativa
+FROM reservas r
+JOIN quartos q ON r.id_quarto = q.id_quarto 
+WHERE r.reserva_ativa = false; 
